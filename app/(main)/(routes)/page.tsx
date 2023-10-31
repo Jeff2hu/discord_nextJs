2c2c507bd00371b6561a7807b4bc018789899439
steven-oss <zxc897286150@gmail.com>
2023-10-31 19:21:21 +0800

    test

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -2,6 +2,7 @@ import { ModeToggle } from "@/components/modeToggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
+  console.log('abc');
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
